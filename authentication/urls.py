b7a1d384a6f4256072de26d222c07f10356ae0e9
lucasfpac <lucasfortunato1@gmail.com>
2024-04-27 19:18:54 +0100

    email validation

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,4 +1,4 @@
-from .views import RegistrationView, LoginView, UsernameValidationView
+from .views import RegistrationView, LoginView, UsernameValidationView, EmailValidationView
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
@@ -6,5 +6,6 @@ from django.views.decorators.csrf import csrf_exempt
 urlpatterns = [
     path('register', RegistrationView.as_view(), name="register"),
     path('validate-username', csrf_exempt(UsernameValidationView.as_view()), name="validate-username"),
+     path('validate-email', csrf_exempt(EmailValidationView.as_view()), name="validate-email"),
     path('login', LoginView.as_view(), name="login"),
 ]
--- expenseswebsite/static/js/register.js
+++ expenseswebsite/static/js/register.js
@@ -1,10 +1,15 @@
 const usernameField = document.querySelector("#usernameField");
-const feedBackArea = document.querySelector(".invalid-feedback");
+const usernameFeedBackArea = document.querySelector(".username-invalid-feedback");
+const emailField = document.querySelector('#emailField');
+const emailFeedBackArea = document.querySelector(".email-invalid-feedback");
+
+
 usernameField.addEventListener("keyup", (e) => {
   const usernameVal = e.target.value;
 
   usernameField.classList.remove("is-invalid");
-  feedBackArea.style.display = "none";
+  usernameField.classList.remove("is-valid");
+  usernameFeedBackArea.style.display = "none";
 
   if (usernameVal.length > 0) {
     fetch("/authentication/validate-username", {
@@ -16,14 +21,49 @@ usernameField.addEventListener("keyup", (e) => {
         console.log("data", data);
         if (data.username_error) {
           usernameField.classList.add("is-invalid");
-          feedBackArea.style.display = "block";
-          feedBackArea.innerHTML = `<p>${data.username_error}</p>`;
+          usernameFeedBackArea.style.display = "block";
+          usernameFeedBackArea.style.color = "red";
+          usernameFeedBackArea.innerHTML = `<p>${data.username_error}</p>`;
         }
         if (data.username_valid) {
           usernameField.classList.add("is-valid");
-          feedBackArea.style.display = "block";
-          feedBackArea.innerHTML = `<p>Username Available</p>`;
+          usernameFeedBackArea.style.display = "block";
+          usernameFeedBackArea.style.color = "green";
+          usernameFeedBackArea.innerHTML = `<p>Username Available</p>`;
         }
+        
       });
   }
 });
+
+emailField.addEventListener("keyup", (e) =>{
+    const emailVal = e.target.value;
+
+    emailField.classList.remove("is-invalid");
+    emailField.classList.remove("is-valid");
+    emailFeedBackArea.style.display = "none";   
+
+    if (emailVal.length > 0) {
+        fetch("/authentication/validate-email", {
+          body: JSON.stringify({ email: emailVal }),
+          method: "POST",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log("data", data);
+            if (data.email_error) {
+              emailField.classList.add("is-invalid");
+              emailFeedBackArea.style.display = "block";
+              emailFeedBackArea.style.color = "red";
+              emailFeedBackArea.innerHTML = `<p>${data.email_error}</p>`;
+            }
+            if (data.email_valid) {
+              emailField.classList.add("is-valid");
+              emailFeedBackArea.style.display = "block";
+              emailFeedBackArea.style.color = "green";
+              emailFeedBackArea.innerHTML = `<p>email Available</p>`;
+            }
+            
+          });
+      }
+})
